8758b3154b7b9886d212ab02b8b172422ece51df
thomasmaeder <thomasmaeder>
2008-03-19 13:28:03 +0000

    make sure bash is used as interpreter allow filter to be adapted (necessary for Cygwin bash)

--- scripts/timing.sh
+++ scripts/timing.sh
@@ -1,16 +1,25 @@
-#! /bin/sh
+#! /bin/bash
 
 # Helper script for repeatedly solving a test file and printing out
 # the time spent in user space
 #
-# Usage: timing.sh <testfile> <nrofruns>
+# Usage: timing.sh <testfile> <nrofruns> [<filter>]
+#
+# <filter> defaults to user which is nice on Linux; on Cygwin, you
+# probably want to use real instead
 #
 # Uses: time, grep
 
 testfile=$1
 nrofruns=$2
+filter=$3
+
+if [ -z $filter ]
+then
+    filter=user
+fi
 
 for ((i=1; $i<=$nrofruns; i=$((i+1))))
 do
     time ./py -maxmem 250M $testfile
-done 2>&1 | grep -w user
+done 2>&1 | grep -w $filter
