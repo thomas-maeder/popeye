f3f7d8fc2cad6a1699f6b81dfb6219f7e38b3ed0
thomasmaeder <thomasmaeder>
2008-05-12 20:16:38 +0000

    fixed a take&make regression introduced from 4.37 to 4.41

--- py4.c
+++ py4.c
@@ -422,7 +422,8 @@ boolean empile(square sq_departure, square sq_arrival, square sq_capture) {
         /* We are generating the make part */
         
         /* Extra rule: pawns must not 'make' to their base line */
-        if (is_pawn(e[sq_departure] && !CondFlag[normalp])
+        if (is_pawn(e[sq_departure])
+            && !CondFlag[normalp]
             && ((e[sq_departure]>0 && sq_arrival<=square_h1)
                 || (e[sq_departure]<0 && sq_arrival>=square_a8)))
           return true;
