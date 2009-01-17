aab4eaa48eee6e3c81df5c9f05214df06874328e
thomasmaeder <thomasmaeder>
2009-01-17 09:28:59 +0000

    *** empty log message ***

---
+++ scripts/checkAgain.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# Check again the example files that produced the output files
+# currently in the current directy.
+#
+# Usage: checkAgain.sh
+#
+# Start from the directory containing the output files of a regression
+# test (which should be a sibling directory of BEISPIEL and EXAMPLES).
+#
+# Uses: sed
+
+for f in *.ref
+do
+    if [ -f $f ]
+    then
+        stem=`echo $f | sed -e 's/[.]ref$//'`
+        inputfile=../EXAMPLES/$stem.inp
+        ../py -maxmem 250M -regression -notrace $inputfile
+    fi
+done
+
+
+for f in *.out
+do
+    if [ -f $f ]
+    then
+        stem=`echo $f | sed -e 's/[.]out$//'`
+        inputfile=../BEISPIEL/$stem.inp
+        ../py -maxmem 250M -regression -notrace $inputfile
+    fi
+done
