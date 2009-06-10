b36b2ac776dc386a1ee2f32fef1b7a1da61663b2
thomasmaeder <thomasmaeder>
2009-06-10 08:33:27 +0000

    changed to implementation of option maxtime using Windows Multimedia timers (-> no MT support needed!)

--- platform/windows32/maxtime.c
+++ platform/windows32/maxtime.c
@@ -1,75 +1,137 @@
 #include "../maxtime.h"
 #include "../../boolean.h"
-#include <limits.h>
-
-void initMaxtime(void)
-{
-  /* no initialization necessary */
-}
-
-#if defined(_MT)
-
-#include <windows.h>
-#include <process.h>
-
-#include <stdio.h>
-
-sig_atomic_t volatile currentProblem = 0;
-
-static void solvingTimeMeasureThread(void *v)
-{
-  unsigned int *seconds = (unsigned int *)v;
-  
-  /*
-   * This function is used by a WIN32-thread to wake up
-   * the thread after WaitTime seconds.
-   */
-  sig_atomic_t const myProblem = currentProblem;
-
-  /* sleep under WIN32 seems to use milliseconds ... */
-  _sleep(*seconds*1000);
-
-  if (myProblem==currentProblem)
-    maxtime_status = MAXTIME_TIMEOUT;
-
-  _endthread();
-}
-
-void setMaxtime(unsigned int *seconds)
-{
-  if (*seconds==UINT_MAX)
-    maxtime_status = MAXTIME_IDLE;
-  else
-  {
-    maxtime_status = MAXTIME_TIMING;
-
-    /* To avoid that a not "used" thread stops Popeye when it times out,
-     * currentProblem is increased every time a new problem is to
-     * be solved.
-     * TODO: kill thread when problem is fully solved before timeout?
-     */
-    ++currentProblem;
-
-    /* this is the reason why we impose a restriction on *seconds in
-     * maxtime.h: *seconds must still have its value when it is read
-     * from within solvingTimeMeasureThread(). */
-    _beginthread(&solvingTimeMeasureThread,0,seconds);
-  }
-}
-
-#else
-
 #include "../../pymsg.h"
 
-void setMaxtime(unsigned int *seconds)
+#include <assert.h>
+#include <limits.h>
+
+/* Implementation of option maxtime using Windows Multimedia timers
+ */
+
+/* number of seconds passed since timer started */
+static sig_atomic_t volatile seconds_counter = 0;
+
+/* number of seconds after which solving is aborted */
+static sig_atomic_t volatile seconds_maxtime = 0;
+
+/* Singular value indicating that an MMRESULT object does not hold an
+ * actual timer resource.
+ * The docs
+ * (http://msdn.microsoft.com/en-us/library/ms713423(VS.85).aspx) say
+ * that timeSetEvent() returns NULL if it fails; but MMRESULT is not a
+ * pointer type ...
+ */
+static MMRESULT const no_timer = 0;
+
+/* id of current Multimedia Timer
+ */
+static MMRESULT current_timer;
+
+/* Actual timer resolution; initialized with intended resolution
+ */
+static unsigned int actual_timer_resolutionMS = 100;
+
+/* Singular value that we use to indicate that the Multimedia Timers
+ * machinery is not available.
+ */
+static unsigned int const MM_timers_not_available = 0;
+
+/* Calibrate timer resolution based on intended resolution and
+ * capabilities indicated by Windows.
+ */
+static void calibrate_timer_resolution(void)
 {
-  if (*seconds<UINT_MAX)
+  TIMECAPS tc;
+  if (timeGetDevCaps(&tc, sizeof tc) == TIMERR_NOERROR) 
+  {
+    if (actual_timer_resolutionMS<tc.wPeriodMin)
+      actual_timer_resolutionMS = tc.wPeriodMin;
+    else if (actual_timer_resolutionMS>tc.wPeriodMax)
+      actual_timer_resolutionMS = tc.wPeriodMax;
+  }
+  else
+    actual_timer_resolutionMS = MM_timers_not_available;
+}
+
+/* Callback function for Multimedia Timer.
+ * Called back by Windows every actual_timer_delayMS milliseconds
+ */
+static void CALLBACK tick(UINT timer_id,      
+                          UINT reserved1,     
+                          DWORD seconds_elapsed,  
+                          DWORD reserved2,     
+                          DWORD reserved3)
+{
+  /* assert(timer_id==current_timer);
+   * is probably not a good idea in (something similar to) a signal
+   * handler ...
+   */
+
+  seconds_counter += seconds_elapsed;
+  if (seconds_counter>=seconds_maxtime)
+    maxtime_status = MAXTIME_TIMEOUT;
+}
+
+/* Attempt to set up a new timer for timing out solving after a number
+ * of seconds. 
+ * @param seconds number of seconds after which solving is aborted
+ */
+static void setupNewTimer(unsigned int seconds)
+{
+  unsigned int const min_seconds_elapsed = actual_timer_resolutionMS*10/1000;
+  DWORD const seconds_elapsed = min_seconds_elapsed==0 ? 1 : min_seconds_elapsed;
+  unsigned int const delayMS = seconds_elapsed*1000;
+
+  /* according to the docs
+   * (http://msdn.microsoft.com/en-us/library/ms713423(VS.85).aspx),
+   * it would be a good idea to also specify TIME_KILL_SYNCHRONOUS,
+   * but the MingW based cross compiler doesn't 'know' that symbol.
+   */
+  current_timer = timeSetEvent(delayMS,actual_timer_resolutionMS,
+                               &tick,seconds_elapsed,
+                               TIME_PERIODIC);
+  if (current_timer==no_timer)
   {
     VerifieMsg(NoMaxTime);
     maxtime_status = MAXTIME_TIMEOUT;
   }
   else
+  {
     maxtime_status = MAXTIME_IDLE;
+    seconds_counter = 0;
+    seconds_maxtime = seconds;
+  }
 }
 
-#endif
+/* Kill previously used timer resource (if any)
+ */
+static void killPreviousTimer(void)
+{
+  if (current_timer!=no_timer)
+  {
+    MMRESULT const kill_result = timeKillEvent(current_timer);
+    assert(kill_result==TIMERR_NOERROR);
+    current_timer = 0;
+  }
+}
+
+void initMaxtime(void)
+{
+  calibrate_timer_resolution();
+  current_timer = no_timer;
+}
+
+void setMaxtime(unsigned int *seconds)
+{
+    killPreviousTimer();
+
+    if (*seconds==UINT_MAX)
+      maxtime_status = MAXTIME_IDLE;
+    else if (actual_timer_resolutionMS==MM_timers_not_available)
+    {
+      VerifieMsg(NoMaxTime);
+      maxtime_status = MAXTIME_TIMEOUT;
+    }
+    else
+      setupNewTimer(*seconds);
+}
