6fef1c6a9acc88374e6b670ddee562fac4c7da25
thomasmaeder <thomasmaeder>
2009-09-05 15:07:48 +0000

    removed an obsolete function

--- pyselfcg.c
+++ pyselfcg.c
@@ -217,28 +217,6 @@ attack_result_type selfcheck_guard_root_defend(table refutations,
   return result;
 }
 
-/* Solve postkey play play after the move that has just
- * been played in the current ply.
- * @param si slice index
- * @param n maximum number of half moves until goal
- */
-boolean self_guard_solve_postkey_in_n(slice_index si, stip_length_type n)
-{
-  boolean result = false;
-  slice_index const next = slices[si].u.pipe.next;
-
-  TraceFunctionEntry(__func__);
-  TraceFunctionParam("%u",si);
-  TraceFunctionParamListEnd();
-
-  direct_defender_solve_postkey_in_n(next,n);
-
-  TraceFunctionExit(__func__);
-  TraceFunctionResult("%u",result);
-  TraceFunctionResultEnd();
-  return result;
-}
-
 /* Solve threats after an attacker's move
  * @param threats table where to add threats
  * @param si slice index
