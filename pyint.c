fa13a1525222efd1426e1ab9e323da211b46f0f4
thomasmaeder <thomasmaeder>
2009-11-11 16:06:28 +0000

    decoupled left moves calculation from STSeriesAdapter

--- pyint.c
+++ pyint.c
@@ -2698,8 +2698,8 @@ static boolean init_moves_left_branch_help(slice_index si,
  * @return true iff the number of moves left have been successfully
  *         initialised for si and its children
  */
-static boolean init_moves_left_series_adapter(slice_index si,
-                                              slice_traversal *st)
+static boolean init_moves_left_branch_series(slice_index si,
+                                             slice_traversal *st)
 {
   boolean result;
   stip_length_type const n = slices[si].u.pipe.u.branch.length;
@@ -2753,7 +2753,7 @@ static slice_operation const moves_left_initialisers[] =
   0,                                /* STBranchDirect */
   0,                                /* STBranchDirectDefender */
   &init_moves_left_branch_help,     /* STBranchHelp */
-  0,                                /* STBranchSeries */
+  &init_moves_left_branch_series,   /* STBranchSeries */
   &init_moves_left_branch_fork,     /* STBranchFork */
   &init_moves_left_leaf_direct,     /* STLeafDirect */
   &init_moves_left_leaf_help,       /* STLeafHelp */
@@ -2768,7 +2768,7 @@ static slice_operation const moves_left_initialisers[] =
   0,                                /* STHelpRoot */
   &slice_traverse_children,         /* STHelpHashed */
   0,                                /* STSeriesRoot */
-  &init_moves_left_series_adapter,  /* STSeriesAdapter */
+  &slice_traverse_children,         /* STSeriesAdapter */
   &slice_traverse_children,         /* STSeriesHashed */
   &slice_traverse_children,         /* STSelfCheckGuard */
   0,                                /* STDirectDefense */
