c3b95616eb1a95ef50795f38f57b80d0421a7072
thomasmaeder <thomasmaeder>
2009-12-05 18:59:48 +0000

    oops

--- pyleaff.c
+++ pyleaff.c
@@ -608,7 +608,7 @@ leaf_forced_root_find_refutations(table refutations,
   }
 
   TraceFunctionExit(__func__);
-  TraceEnumerator(quantity_of_refutations_type,result,"");
+  TraceEnumerator("%u",result);
   TraceFunctionResultEnd();
   return result;
 }
