fe711a38db94bad21986dcd59ea77a3c61e594ed
thomasmaeder <thomasmaeder>
2011-06-14 16:49:44 +0000

    more robust method for detecting whether a series branch is an intro series

--- output/plaintext/line/line.c
+++ output/plaintext/line/line.c
@@ -10,6 +10,7 @@
 #include "trace.h"
 
 #include <assert.h>
+#include <limits.h>
 
 static void instrument_suppressor(slice_index si, stip_structure_traversal *st)
 {
@@ -63,33 +64,105 @@ static void instrument_root(slice_index si, stip_structure_traversal *st)
   TraceFunctionResultEnd();
 }
 
-/* Determine whether an end of branch slice ends an intro series
- * @param end_of_branch identifies end of branch slice
- * @return true iff end_of_branch ends an intro series
- */
-static boolean ends_intro_series(slice_index end_of_branch)
+static void min_distance_to_goal_battle(slice_index si,
+                                        stip_structure_traversal *st)
 {
-  slice_index const fork = slices[end_of_branch].u.fork.fork;
-  boolean result = false;
+  stip_length_type * const min_distance_to_goal = st->param;
+  stip_length_type const min_dist = slices[si].u.branch.length-slack_length_battle;
+
+  TraceFunctionEntry(__func__);
+  TraceFunctionParam("%u",si);
+  TraceFunctionParamListEnd();
+
+  if (min_dist<*min_distance_to_goal)
+    *min_distance_to_goal = min_dist;
+
+  TraceFunctionExit(__func__);
+  TraceFunctionResultEnd();
+}
+
+static void min_distance_to_goal_help(slice_index si,
+                                        stip_structure_traversal *st)
+{
+  stip_length_type * const min_distance_to_goal = st->param;
+  stip_length_type const min_dist = slices[si].u.branch.length-slack_length_help;
+
+  TraceFunctionEntry(__func__);
+  TraceFunctionParam("%u",si);
+  TraceFunctionParamListEnd();
+
+  if (min_dist<*min_distance_to_goal)
+    *min_distance_to_goal = min_dist;
+
+  TraceFunctionExit(__func__);
+  TraceFunctionResultEnd();
+}
+
+static void min_distance_to_goal_series(slice_index si,
+                                        stip_structure_traversal *st)
+{
+  stip_length_type * const min_distance_to_goal = st->param;
+  stip_length_type const min_dist = slices[si].u.branch.length-slack_length_series;
+
+  TraceFunctionEntry(__func__);
+  TraceFunctionParam("%u",si);
+  TraceFunctionParamListEnd();
+
+  if (min_dist<*min_distance_to_goal)
+    *min_distance_to_goal = min_dist;
+
+  TraceFunctionExit(__func__);
+  TraceFunctionResultEnd();
+}
+
+static void min_distance_to_goal_goal(slice_index si,
+                                        stip_structure_traversal *st)
+{
+  stip_length_type * const min_distance_to_goal = st->param;
+
+  TraceFunctionEntry(__func__);
+  TraceFunctionParam("%u",si);
+  TraceFunctionParamListEnd();
+
+  *min_distance_to_goal = 0;
+
+  TraceFunctionExit(__func__);
+  TraceFunctionResultEnd();
+}
+
+structure_traversers_visitors const min_distance_to_goal_finders[] =
+{
+  { STAttackAdapter,     &min_distance_to_goal_battle },
+  { STDefenseAdapter,    &min_distance_to_goal_battle },
+  { STHelpAdapter,       &min_distance_to_goal_help   },
+  { STSeriesAdapter,     &min_distance_to_goal_series },
+  { STGoalReachedTester, &min_distance_to_goal_goal   }
+};
+
+enum
+{
+  nr_min_distance_to_goal_finders = sizeof min_distance_to_goal_finders
+                                    / sizeof min_distance_to_goal_finders[0]
+};
+
+/* Determine minimum distance (in half moves) to goal
+ * @param end_of_branch identifies end of branch slice
+ * @return minimum distance
+ */
+static stip_length_type min_distance_to_goal(slice_index end_of_branch)
+{
+  stip_length_type result = UINT_MAX;
+  stip_structure_traversal st;
 
   TraceFunctionEntry(__func__);
   TraceFunctionParam("%u",end_of_branch);
   TraceFunctionParamListEnd();
 
-  if (branch_find_slice(STSeriesAdapter,fork)!=no_slice)
-    result = true;
-  else
-  {
-    slice_index const attack = branch_find_slice(STAttackAdapter,fork);
-    if (attack!=no_slice && slices[attack].u.branch.length>slack_length_battle+1)
-      result = true;
-    else
-    {
-      slice_index const help = branch_find_slice(STHelpAdapter,fork);
-      if (help!=no_slice && slices[help].u.branch.length>slack_length_help+1)
-        result = true;
-    }
-  }
+  stip_structure_traversal_init(&st,&result);
+  stip_structure_traversal_override(&st,
+                                    min_distance_to_goal_finders,
+                                    nr_min_distance_to_goal_finders);
+  stip_traverse_structure(slices[end_of_branch].u.fork.fork,&st);
 
   TraceFunctionExit(__func__);
   TraceFunctionResult("%u",result);
@@ -106,7 +179,7 @@ static void instrument_end_of_branch(slice_index si,
   TraceFunctionParam("%u",si);
   TraceFunctionParamListEnd();
 
-  if (ends_intro_series(si))
+  if (min_distance_to_goal(si)>1)
   {
     slice_index const marker
         = alloc_output_plaintext_line_end_of_intro_series_marker_slice();
