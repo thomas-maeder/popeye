37ad47f647f1b7fb953373ad824d732b9385655a
stephenemmerson <stephenemmerson>
2011-06-18 08:26:26 +0000

    contraparrain bug/incomplete implementation - correction

--- py1.c
+++ py1.c
@@ -781,19 +781,6 @@ boolean nocontact(square sq_departure, square sq_arrival, square sq_capture, noc
       }
     }
 
-    if (CondFlag[contraparrain] && pprise[parent_ply[parent_ply[nbply]]] != vide)
-    {
-      cr = (move_generation_stack[repere[parent_ply[nbply]]].capture
-            - sq_arrival + sq_departure);
-      pc = e[cr];
-      if (pc==vide)
-      {
-        e[cr]= pprise[parent_ply[parent_ply[nbply]]];
-        TraceSquare(cr);
-        TraceText("\n");
-      }
-    }
-
     if (pp != vide && pp != obs) {
       if (anycirce && abs(pp) > roib && !anyparrain) {
         /* This still doesn't work with neutral pieces.
