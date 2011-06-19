79c449b2c6d705d90239269851ad2de22299e4ce
thomasmaeder <thomasmaeder>
2011-06-19 14:12:30 +0000

    got rid of function series_branch_make_root_slices() (duplicated in help play)

--- stipulation/series_play/branch.c
+++ stipulation/series_play/branch.c
@@ -1,18 +1,11 @@
 #include "stipulation/series_play/branch.h"
-#include "pyslice.h"
 #include "pypipe.h"
 #include "stipulation/constraint.h"
 #include "stipulation/branch.h"
-#include "stipulation/proxy.h"
-#include "stipulation/dead_end.h"
-#include "stipulation/end_of_branch.h"
-#include "stipulation/end_of_branch_goal.h"
 #include "stipulation/move.h"
 #include "stipulation/dummy_move.h"
-#include "stipulation/boolean/binary.h"
 #include "stipulation/series_play/adapter.h"
 #include "stipulation/help_play/branch.h"
-#include "stipulation/goals/goals.h"
 #include "trace.h"
 
 #include <assert.h>
@@ -57,56 +50,6 @@ slice_index alloc_series_branch(stip_length_type length,
   return result;
 }
 
-static void serve_as_root_hook(slice_index si, stip_structure_traversal *st)
-{
-  slice_index * const root_slice = st->param;
-
-  TraceFunctionEntry(__func__);
-  TraceFunctionParam("%u",si);
-  TraceFunctionParamListEnd();
-
-  *root_slice = si;
-
-  TraceFunctionExit(__func__);
-  TraceFunctionResultEnd();
-}
-
-/* Create the root slices of a series branch
- * @param adapter identifies the adapter slice at the beginning of the branch
- * @return identifier of initial root slice
- */
-static slice_index series_branch_make_root_slices(slice_index adapter)
-{
-  slice_index result = no_slice;
-
-  TraceFunctionEntry(__func__);
-  TraceFunctionParam("%u",adapter);
-  TraceFunctionParamListEnd();
-
-  {
-    stip_structure_traversal st;
-    slice_structural_type i;
-
-    TraceStipulation(adapter);
-
-    stip_structure_traversal_init(&st,&result);
-    for (i = 0; i!=nr_slice_structure_types; ++i)
-      if (slice_structure_is_subclass(i,slice_structure_pipe))
-        stip_structure_traversal_override_by_structure(&st,i,&pipe_make_root);
-      else if (slice_structure_is_subclass(i,slice_structure_binary))
-        stip_structure_traversal_override_by_structure(&st,i,&binary_make_root);
-    stip_structure_traversal_override_single(&st,STEndOfRoot,&serve_as_root_hook);
-    stip_traverse_structure(adapter,&st);
-
-    TraceStipulation(result);
-  }
-
-  TraceFunctionExit(__func__);
-  TraceFunctionResult("%u",result);
-  TraceFunctionResultEnd();
-  return result;
-}
-
 /* Produce slices representing set play.
  * @param adapter identifies the adapter slice at the beginning of the branch
  * @return entry point of the slices representing set play
@@ -127,10 +70,7 @@ slice_index series_make_setplay(slice_index adapter)
       alloc_series_adapter_slice(slack_length_help,slack_length_help),
       alloc_pipe(STEndOfRoot)
     };
-    enum
-    {
-      nr_prototypes = sizeof prototypes / sizeof prototypes[0]
-    };
+    enum { nr_prototypes = sizeof prototypes / sizeof prototypes[0] };
     help_branch_insert_slices(next,prototypes,nr_prototypes);
 
     {
@@ -139,7 +79,7 @@ slice_index series_make_setplay(slice_index adapter)
       if (slices[slices[set_adapter].u.pipe.next].type==STDeadEnd)
         ; /* set play not applicable */
       else
-        result = series_branch_make_root_slices(set_adapter);
+        result = help_branch_make_root_slices(set_adapter);
       pipe_remove(set_adapter);
     }
   }
