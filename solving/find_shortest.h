088bb82ca88d97a5db0814d1e7bc3c1eafcb0ede
thomasmaeder <thomasmaeder>
2012-02-25 19:13:01 +0000

    fixed a comment

--- solving/find_shortest.h
+++ solving/find_shortest.h
@@ -26,13 +26,13 @@ slice_index alloc_find_shortest_slice(stip_length_type length,
  */
 stip_length_type find_shortest_attack(slice_index si, stip_length_type n);
 
-/* Determine and write the solution(s) in a help stipulation
- * @param si slice index
- * @param n exact number of moves to reach the end state
+/* Solve in a number of half-moves
+ * @param si identifies slice
+ * @param n exact number of half moves until end state has to be reached
  * @return length of solution found, i.e.:
- *         n+2 the move leading to the current position has turned out
+ *         n+4 the move leading to the current position has turned out
  *             to be illegal
- *         n+1 no solution found
+ *         n+2 no solution found
  *         n   solution found
  */
 stip_length_type find_shortest_help(slice_index si, stip_length_type n);
