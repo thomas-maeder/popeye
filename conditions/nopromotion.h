62be40bc407ecd653ea6fd2addeb72d1303b1a3c
thomasmaeder <thomasmaeder>
2012-12-15 07:43:12 +0000

    oops

--- conditions/nopromotion.h
+++ conditions/nopromotion.h
@@ -16,6 +16,17 @@
 stip_length_type nopromotion_avoid_promotion_moving_solve(slice_index si,
                                                           stip_length_type n);
 
+/* Try to solve in n half-moves.
+ * @param si slice index
+ * @param n maximum number of half moves
+ * @return length of solution found and written, i.e.:
+ *            slack_length-2 the move just played or being played is illegal
+ *            <=n length of shortest solution found
+ *            n+2 no solution found
+ */
+stip_length_type nopromotion_avoid_promotion_reborn_solve(slice_index si,
+                                                          stip_length_type n);
+
 /* Instrument the solvers with NoPromotions
  * @param si identifies the root slice of the stipulation
  */
