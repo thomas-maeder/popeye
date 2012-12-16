0ddb77a8af19af494c16cf4cafc36a99e1e6f330
thomasmaeder <thomasmaeder>
2012-12-16 17:00:18 +0000

    added some missing #includes

--- conditions/messigny.c
+++ conditions/messigny.c
@@ -1,8 +1,11 @@
 #include "conditions/messigny.h"
+#include "pydata.h"
 #include "solving/castling.h"
 #include "solving/move_effect_journal.h"
-#include "pydata.h"
+#include "solving/move_generator.h"
 #include "stipulation/stipulation.h"
+#include "stipulation/pipe.h"
+#include "stipulation/branch.h"
 #include "debugging/trace.h"
 
 #include <assert.h>
