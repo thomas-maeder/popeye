61aca4e74e1b42c88190911b1a80e3b2574f57db
thomasmaeder <thomasmaeder>
2013-08-06 21:34:45 +0000

    Back Home move filtering now corresponds to Back Home observation validation

--- conditions/backhome.h
+++ conditions/backhome.h
@@ -10,6 +10,23 @@
  */
 boolean backhome_existance_tester_validate_observation(slice_index si);
 
+
+/* Try to solve in n half-moves.
+ * @param si slice index
+ * @param n maximum number of half moves
+ * @return length of solution found and written, i.e.:
+ *            previous_move_is_illegal the move just played (or being played)
+ *                                     is illegal
+ *            immobility_on_next_move  the moves just played led to an
+ *                                     unintended immobility on the next move
+ *            <=n+1 length of shortest solution found (n+1 only if in next
+ *                                     branch)
+ *            n+2 no solution found in this branch
+ *            n+3 no solution found in next branch
+ */
+stip_length_type backhome_existance_tester_solve(slice_index si,
+                                                 stip_length_type n);
+
 /* Validate an observation according to Back Home
  * @return true iff the observation is valid
  */
