97fb07ac4225e4772550290d3045408ed8c98465
thomasmaeder <thomasmaeder>
2014-02-28 12:20:44 +0000

    added two forgotten function declarations

--- conditions/circe/parrain.h
+++ conditions/circe/parrain.h
@@ -3,6 +3,23 @@
 
 #include "solving/solve.h"
 
+
+/* Try to solve in n half-moves.
+ * @param si slice index
+ * @param n maximum number of half moves
+ * @return length of solution found and written, i.e.:
+ *            previous_move_is_illegal the move just played is illegal
+ *            this_move_is_illegal     the move being played is illegal
+ *            immobility_on_next_move  the moves just played led to an
+ *                                     unintended immobility on the next move
+ *            <=n+1 length of shortest solution found (n+1 only if in next
+ *                                     branch)
+ *            n+2 no solution found in this branch
+ *            n+3 no solution found in next branch
+ */
+stip_length_type circe_parrain_initalise_from_capture_in_last_move_solve(slice_index si,
+                                                                         stip_length_type n);
+
 /* Try to solve in n half-moves.
  * @param si slice index
  * @param n maximum number of half moves
@@ -18,5 +35,22 @@
  */
 stip_length_type circe_parrain_determine_rebirth_solve(slice_index si,
                                                        stip_length_type n);
+
+
+/* Try to solve in n half-moves.
+ * @param si slice index
+ * @param n maximum number of half moves
+ * @return length of solution found and written, i.e.:
+ *            previous_move_is_illegal the move just played is illegal
+ *            this_move_is_illegal     the move being played is illegal
+ *            immobility_on_next_move  the moves just played led to an
+ *                                     unintended immobility on the next move
+ *            <=n+1 length of shortest solution found (n+1 only if in next
+ *                                     branch)
+ *            n+2 no solution found in this branch
+ *            n+3 no solution found in next branch
+ */
+stip_length_type circe_parrain_capture_fork_solve(slice_index si,
+                                                  stip_length_type n);
 
 #endif
