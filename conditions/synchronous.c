3b0fd7d7cba2441decdd07e781ed61884178a50e
Thomas Maeder <maeder+github@glue.ch>
2015-07-21 21:52:50 +0200

    simplification

--- conditions/synchronous.c
+++ conditions/synchronous.c
@@ -14,29 +14,23 @@ int synchronous_measure_length(void)
 {
   int result;
   ply const parent = parent_ply[nbply];
+  square const sq_departure = move_generation_stack[CURRMOVE_OF_PLY(nbply)].departure;
+  square const sq_arrival = move_generation_stack[CURRMOVE_OF_PLY(nbply)].arrival;
+
+  move_effect_journal_index_type const parent_base = move_effect_journal_base[parent];
+  move_effect_journal_index_type const movement = parent_base+move_effect_journal_index_offset_movement;
+  move_effect_journal_index_type const parent_top = move_effect_journal_base[parent+1];
 
   TraceFunctionEntry(__func__);
   TraceFunctionParamListEnd();
 
-  if (parent>ply_retro_move)
+  if (movement<parent_top)
   {
-    square const sq_departure = move_generation_stack[CURRMOVE_OF_PLY(nbply)].departure;
-    square const sq_arrival = move_generation_stack[CURRMOVE_OF_PLY(nbply)].arrival;
-
-    move_effect_journal_index_type const parent_base = move_effect_journal_base[parent];
-    move_effect_journal_index_type const movement = parent_base+move_effect_journal_index_offset_movement;
-    move_effect_journal_index_type const parent_top = move_effect_journal_base[parent+1];
-
-    if (movement<parent_top)
-    {
-      square const sq_parent_departure = move_effect_journal_get_departure_square(parent);
-      square const sq_parent_arrival = move_effect_journal[movement].u.piece_movement.to;
-      numvec const parent_diff = sq_parent_departure-sq_parent_arrival;
-      numvec const diff = sq_departure-sq_arrival;
-      result = diff==parent_diff;
-    }
-    else
-      result = true;
+    square const sq_parent_departure = move_effect_journal_get_departure_square(parent);
+    square const sq_parent_arrival = move_effect_journal[movement].u.piece_movement.to;
+    numvec const parent_diff = sq_parent_departure-sq_parent_arrival;
+    numvec const diff = sq_departure-sq_arrival;
+    result = diff==parent_diff;
   }
   else
     result = true;
@@ -55,29 +49,23 @@ int antisynchronous_measure_length(void)
 {
   int result;
   ply const parent = parent_ply[nbply];
+  square const sq_departure = move_generation_stack[CURRMOVE_OF_PLY(nbply)].departure;
+  square const sq_arrival = move_generation_stack[CURRMOVE_OF_PLY(nbply)].arrival;
+
+  move_effect_journal_index_type const parent_base = move_effect_journal_base[parent];
+  move_effect_journal_index_type const movement = parent_base+move_effect_journal_index_offset_movement;
+  move_effect_journal_index_type const parent_top = move_effect_journal_base[parent+1];
 
   TraceFunctionEntry(__func__);
   TraceFunctionParamListEnd();
 
-  if (parent>ply_retro_move)
+  if (movement<parent_top)
   {
-    square const sq_departure = move_generation_stack[CURRMOVE_OF_PLY(nbply)].departure;
-    square const sq_arrival = move_generation_stack[CURRMOVE_OF_PLY(nbply)].arrival;
-
-    move_effect_journal_index_type const parent_base = move_effect_journal_base[parent];
-    move_effect_journal_index_type const movement = parent_base+move_effect_journal_index_offset_movement;
-    move_effect_journal_index_type const parent_top = move_effect_journal_base[parent+1];
-
-    if (movement<parent_top)
-    {
-      square const sq_parent_departure = move_effect_journal_get_departure_square(parent);
-      square const sq_parent_arrival = move_effect_journal[movement].u.piece_movement.to;
-      numvec const parent_diff = sq_parent_departure-sq_parent_arrival;
-      numvec const diff = sq_departure-sq_arrival;
-      result = diff==-parent_diff;
-    }
-    else
-      result = true;
+    square const sq_parent_departure = move_effect_journal_get_departure_square(parent);
+    square const sq_parent_arrival = move_effect_journal[movement].u.piece_movement.to;
+    numvec const parent_diff = sq_parent_departure-sq_parent_arrival;
+    numvec const diff = sq_departure-sq_arrival;
+    result = diff==-parent_diff;
   }
   else
     result = true;
