decdeec5127bdc4ebe1541be353019b857baef5c
Thomas Maeder <maeder+github@glue.ch>
2015-07-22 08:36:51 +0200

    adapted to the slight change in move_effect_journal_get_departure_square()

--- conditions/synchronous.c
+++ conditions/synchronous.c
@@ -14,26 +14,26 @@ int synchronous_measure_length(void)
 {
   int result;
   ply const parent = parent_ply[nbply];
-  square const sq_departure = move_generation_stack[CURRMOVE_OF_PLY(nbply)].departure;
-  square const sq_arrival = move_generation_stack[CURRMOVE_OF_PLY(nbply)].arrival;
-
-  move_effect_journal_index_type const parent_base = move_effect_journal_base[parent];
-  move_effect_journal_index_type const movement = parent_base+move_effect_journal_index_offset_movement;
-  move_effect_journal_index_type const parent_top = move_effect_journal_base[parent+1];
+  square const sq_parent_departure = move_effect_journal_get_departure_square(parent);
 
   TraceFunctionEntry(__func__);
   TraceFunctionParamListEnd();
 
-  if (movement<parent_top)
+  if (sq_parent_departure==initsquare)
+    result = true;
+  else
   {
-    square const sq_parent_departure = move_effect_journal_get_departure_square(parent);
-    square const sq_parent_arrival = move_effect_journal[movement].u.piece_movement.to;
+    move_effect_journal_index_type const parent_base = move_effect_journal_base[parent];
+    move_effect_journal_index_type const parent_movement = parent_base+move_effect_journal_index_offset_movement;
+    square const sq_parent_arrival = move_effect_journal[parent_movement].u.piece_movement.to;
     numvec const parent_diff = sq_parent_departure-sq_parent_arrival;
+
+    square const sq_departure = move_generation_stack[CURRMOVE_OF_PLY(nbply)].departure;
+    square const sq_arrival = move_generation_stack[CURRMOVE_OF_PLY(nbply)].arrival;
     numvec const diff = sq_departure-sq_arrival;
+
     result = diff==parent_diff;
   }
-  else
-    result = true;
 
   TraceFunctionExit(__func__);
   TraceFunctionResult("%u",result);
@@ -49,26 +49,25 @@ int antisynchronous_measure_length(void)
 {
   int result;
   ply const parent = parent_ply[nbply];
-  square const sq_departure = move_generation_stack[CURRMOVE_OF_PLY(nbply)].departure;
-  square const sq_arrival = move_generation_stack[CURRMOVE_OF_PLY(nbply)].arrival;
-
-  move_effect_journal_index_type const parent_base = move_effect_journal_base[parent];
-  move_effect_journal_index_type const movement = parent_base+move_effect_journal_index_offset_movement;
-  move_effect_journal_index_type const parent_top = move_effect_journal_base[parent+1];
+  square const sq_parent_departure = move_effect_journal_get_departure_square(parent);
 
   TraceFunctionEntry(__func__);
   TraceFunctionParamListEnd();
 
-  if (movement<parent_top)
+  if (sq_parent_departure==initsquare)
+    result = true;
+  else
   {
-    square const sq_parent_departure = move_effect_journal_get_departure_square(parent);
-    square const sq_parent_arrival = move_effect_journal[movement].u.piece_movement.to;
+    move_effect_journal_index_type const parent_base = move_effect_journal_base[parent];
+    move_effect_journal_index_type const parent_movement = parent_base+move_effect_journal_index_offset_movement;
+    square const sq_parent_arrival = move_effect_journal[parent_movement].u.piece_movement.to;
     numvec const parent_diff = sq_parent_departure-sq_parent_arrival;
+
+    square const sq_departure = move_generation_stack[CURRMOVE_OF_PLY(nbply)].departure;
+    square const sq_arrival = move_generation_stack[CURRMOVE_OF_PLY(nbply)].arrival;
     numvec const diff = sq_departure-sq_arrival;
     result = diff==-parent_diff;
   }
-  else
-    result = true;
 
   TraceFunctionExit(__func__);
   TraceFunctionResult("%u",result);
