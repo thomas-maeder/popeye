6a0742ade0ee1d03c8da7d5d49e99a1f797d2e20
Thomas Maeder <maeder@glue.ch>
2015-09-19 23:37:01 +0200

    added forgotten function declarations

--- optimisations/intelligent/limit_nr_solutions_per_target.h
+++ optimisations/intelligent/limit_nr_solutions_per_target.h
@@ -11,6 +11,22 @@
  * @param si identifies the slice where to start instrumenting
  */
 void intelligent_nr_solutions_per_target_position_solving_instrumenter_solve(slice_index si);
+void intelligent_nr_solutions_per_target_position_problem_instrumenter_solve(slice_index si);
+
+/* Try to solve in solve_nr_remaining half-moves.
+ * @param si slice index
+ * @note assigns solve_result the length of solution found and written, i.e.:
+ *            previous_move_is_illegal the move just played is illegal
+ *            this_move_is_illegal     the move being played is illegal
+ *            immobility_on_next_move  the moves just played led to an
+ *                                     unintended immobility on the next move
+ *            <=n+1 length of shortest solution found (n+1 only if in next
+ *                                     branch)
+ *            n+2 no solution found in this branch
+ *            n+3 no solution found in next branch
+ *            (with n denominating solve_nr_remaining)
+ */
+void intelligent_nr_solutions_per_target_position_initialiser_solve(slice_index si);
 
 /* Instrument the solving machinery with option intelligent n
  * @param si identifies the slice where to start instrumenting
