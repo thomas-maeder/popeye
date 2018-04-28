6363bff01818126d701f39eb606be825ce59c86e
Thomas Maeder <maeder+github@glue.ch>
2018-04-28 17:35:14 +0200

    adapted to machines with more cores

--- scripts/checkAgain.sh
+++ scripts/checkAgain.sh
@@ -20,7 +20,7 @@ _cmd="${POPEYEDIR}/py -maxmem 1G -maxtrace 0 -regression"
 #_cmd="WINEPREFIX=~/.wine64 ${POPEYEDIR}/pywin64.exe -maxmem 1G -maxtrace 0 -regression"
 
 # number of processors
-PMAX=3
+PMAX=6
 
 (for f in *.tst
 do
