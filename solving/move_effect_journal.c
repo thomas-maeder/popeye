0d18ccccd8e183404adedbe77205cc40a48276af
Thomas Maeder <maeder+github@glue.ch>
2018-12-01 20:53:49 +0100

    cosmetics

--- solving/move_effect_journal.c
+++ solving/move_effect_journal.c
@@ -1260,22 +1260,20 @@ static void undo_input_condition(move_effect_journal_entry_type const *entry)
 
   assert(nbply==ply_twinning || nbply==ply_diagram_setup);
 
+  InitCond();
+
   /* restore the original condition (if any) if we are undoing a
    * condition twinning
    */
   if (nbply==ply_twinning)
   {
-    InitCond();
-
+    move_effect_journal_index_type const idx_cond = find_original_condition();
+    if (idx_cond!=move_effect_journal_index_null)
     {
-      move_effect_journal_index_type const idx_cond = find_original_condition();
-      if (idx_cond!=move_effect_journal_index_null)
-      {
-        move_effect_journal_entry_type const * const cond = &move_effect_journal[idx_cond];
-        InputStartReplay(cond->u.input_complex.start);
-        ParseCond(ReadNextTokStr());
-        InputEndReplay();
-      }
+      move_effect_journal_entry_type const * const cond = &move_effect_journal[idx_cond];
+      InputStartReplay(cond->u.input_complex.start);
+      ParseCond(ReadNextTokStr());
+      InputEndReplay();
     }
   }
 
