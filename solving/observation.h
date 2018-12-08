7d5d4892d5760dc0e22c67bb04e56e4a3cb43b59
Thomas Maeder <maeder+github@glue.ch>
2018-12-08 19:12:55 +0100

    added a forgotten #include

--- solving/observation.h
+++ solving/observation.h
@@ -112,6 +112,7 @@ typedef boolean (*validator_id)(void);
 /* version with slice indices */
 #include "solving/temporary_hacks.h"
 typedef slice_index validator_id;
+#include "solving/fork.h"
 #define EVALUATE(key) (temporary_hack_##key##_validator[trait[nbply]])
 #define INVOKE_EVALUATE(validator) (fork_validate_observation_recursive_delegate(validator))
 
