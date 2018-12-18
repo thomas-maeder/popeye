804ec7716f39d0cf607f120dce7762f53b00a05c
Thomas Maeder <maeder+github@glue.ch>
2018-12-18 23:24:58 +0100

    castling as make now works

--- conditions/make_and_take.c
+++ conditions/make_and_take.c
@@ -322,6 +322,19 @@ void make_and_take_generate_captures_by_walk_solve(slice_index si)
   TraceFunctionResultEnd();
 }
 
+/* Try to solve in solve_nr_remaining half-moves.
+ * @param si slice index
+ * @note assigns solve_result the length of solution found and written, i.e.:
+ *            previous_move_is_illegal the move just played is illegal
+ *            this_move_is_illegal     the move being played is illegal
+ *            immobility_on_next_move  the moves just played led to an
+ *                                     unintended immobility on the next move
+ *            <=n+1 length of shortest solution found (n+1 only if in next
+ *                                     branch)
+ *            n+2 no solution found in this branch
+ *            n+3 no solution found in next branch
+ *            (with n denominating solve_nr_remaining)
+ */
 void make_and_take_move_castling_partner(slice_index si)
 {
   TraceFunctionEntry(__func__);
@@ -377,6 +390,11 @@ void make_and_take_reset(void)
   TraceFunctionResultEnd();
 }
 
+/* Determine whether a particular move starts with castling as a make part
+ * @param ply the ply that the move is played in
+ * @param move_id the id of the move
+ * @return kingside_castling, queenside_castling or initsquare
+ */
 square make_and_take_has_move_castling_as_make(ply ply, numecoup move_id)
 {
   square result;
--- conditions/make_and_take.h
+++ conditions/make_and_take.h
@@ -30,8 +30,26 @@ make_and_take_limit_move_generation_make_walk_is_in_check(slice_index si,
  */
 void make_and_take_generate_captures_by_walk_solve(slice_index si);
 
+/* Try to solve in solve_nr_remaining half-moves.
+ * @param si slice index
+ * @note assigns solve_result the length of solution found and written, i.e.:
+ *            previous_move_is_illegal the move just played is illegal
+ *            this_move_is_illegal     the move being played is illegal
+ *            immobility_on_next_move  the moves just played led to an
+ *                                     unintended immobility on the next move
+ *            <=n+1 length of shortest solution found (n+1 only if in next
+ *                                     branch)
+ *            n+2 no solution found in this branch
+ *            n+3 no solution found in next branch
+ *            (with n denominating solve_nr_remaining)
+ */
 void make_and_take_move_castling_partner(slice_index si);
 
+/* Determine whether a particular move starts with castling as a make part
+ * @param ply the ply that the move is played in
+ * @param move_id the id of the move
+ * @return kingside_castling, queenside_castling or initsquare
+ */
 square make_and_take_has_move_castling_as_make(ply ply, numecoup move_id);
 
 /* Instrument the solvers with Make&Take
