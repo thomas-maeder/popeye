97eed4ea7ddcaabc9ca3ad633f6879e5b6f3cc49
Thomas Maeder <maeder+github@glue.ch>
2019-04-17 20:05:58 +0200

    trace values that the following assertion tests

--- position/effects/flags_change.c
+++ position/effects/flags_change.c
@@ -48,6 +48,8 @@ static void undo_flags_change(move_effect_journal_entry_type const *entry)
   TraceFunctionParamListEnd();
 
   TraceSquare(on);
+  TraceValue("%x",being_solved.spec[on]);
+  TraceValue("%x",entry->u.flags_change.to);
   TraceEOL();
 
   assert(being_solved.spec[on]==entry->u.flags_change.to);
