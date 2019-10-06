cb65111c8c1e6ad04ca934e5ef3384866cc1b2fa
Thomas Maeder <maeder+github@glue.ch>
2019-10-06 20:28:39 +0200

    modulariation with code related to validation when the goal should be reached

--- solving/machinery/dispatch.c
+++ solving/machinery/dispatch.c
@@ -246,6 +246,7 @@
 #include "pieces/attributes/total_invisible.h"
 #include "pieces/attributes/total_invisible/move_generation.h"
 #include "pieces/attributes/total_invisible/revelations.h"
+#include "pieces/attributes/total_invisible/goal_validation.h"
 #include "pieces/attributes/uncapturable.h"
 #include "pieces/attributes/kamikaze/kamikaze.h"
 #include "pieces/walks/generate_moves.h"
