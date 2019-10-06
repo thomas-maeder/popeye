156eef894dfdbabf2ea0894a355c691cb3785113
Thomas Maeder <maeder+github@glue.ch>
2019-10-06 20:43:45 +0200

    modulariation with code related to the interception of illegal checks by invisible pieces

--- pieces/attributes/total_invisible/uninterceptable_check.h
+++ pieces/attributes/total_invisible/uninterceptable_check.h
@@ -2,6 +2,22 @@
 #define PIECES_ATTRIBUTES_TOTAL_INVISIBLE_UNINTERCEPTABLE_CHECK_H
 
 #include "position/position.h"
+#include "stipulation/stipulation.h"
+
+/* Try to solve in solve_nr_remaining half-moves.
+ * @param si slice index
+ * @note assigns solve_result the length of solution found and written, i.e.:
+ *            previous_move_is_illegal the move just played is illegal
+ *            this_move_is_illegal     the move being played is illegal
+ *            immobility_on_next_move  the moves just played led to an
+ *                                     unintended immobility on the next move
+ *            <=n+1 length of shortest solution found (n+1 only if in next
+ *                                     branch)
+ *            n+2 no solution found in this branch
+ *            n+3 no solution found in next branch
+ *            (with n denominating solve_nr_remaining)
+ */
+void total_invisible_uninterceptable_selfcheck_guard_solve(slice_index si);
 
 vec_index_type is_rider_check_uninterceptable(Side side_checking, square king_pos,
                                               vec_index_type kanf, vec_index_type kend, piece_walk_type rider_walk);
