d9f6f9513a70ab35e3278d627f33af8c1631f096
Joshua Green <jvg1981@aim.com>
2020-04-27 22:13:51 -0400

    Replacing a macro with an enum.

--- solving/machinery/twin.h
+++ solving/machinery/twin.h
@@ -75,6 +75,9 @@ void pieces_counter_solve(slice_index si);
 
 static char const TWIN_LABELS[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                                    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-#define NUM_TWIN_LABELS ((sizeof TWIN_LABELS)/(sizeof *TWIN_LABELS))
+enum
+{
+  NUM_TWIN_LABELS = ((sizeof TWIN_LABELS)/(sizeof *TWIN_LABELS))
+};
 
 #endif
