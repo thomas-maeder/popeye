e4f76e862619b8840b0e1cbab8df8e791ab5508e
Joshua Green <jvg1981@aim.com>
2020-05-08 16:13:15 -0400

    Adding a TODO.  (And yes, that behavior existed before my edits, though one branch used to lead to a call to exit.)

--- DHT/fxf.h
+++ DHT/fxf.h
@@ -3,7 +3,8 @@
 
 #include <stddef.h>
 
-int fxfInit(size_t GlobalSize);
+int fxfInit(size_t GlobalSize); /* TODO: This currently returns -1 in one specific error case and 0 in all other cases.
+                                         Should it signal other error cases? */
 int fxfInitialised(void);
 void *fxfAlloc(size_t size);
 void *fxfReAlloc(void *ptr, size_t OldSize, size_t NewSize);
