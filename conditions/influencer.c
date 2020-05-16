707c9bcf185a2f429be1731ad94bb87582196945
Thomas Maeder <maeder+github@glue.ch>
2020-05-16 19:04:31 +0200

    added an example with Messigny Chess

--- conditions/influencer.c
+++ conditions/influencer.c
@@ -9,6 +9,49 @@
 #include "debugging/trace.h"
 #include "debugging/assert.h"
 
+static void apply_influence(PieceIdType id_influencer, square sq_to)
+{
+  Side const side_playing = trait[nbply];
+  Side const side_influenced = advers(side_playing);
+  SquareFlags const sq_prom = side_influenced==White ? WhPromSq : BlPromSq;
+  SquareFlags const sq_base = side_influenced==White ? WhBaseSq : BlBaseSq;
+
+  TraceFunctionEntry(__func__);
+  TraceFunctionParam("%u",id_influencer);
+  TraceSquare(sq_to);
+  TraceFunctionParamListEnd();
+
+  {
+    square const sq_arrival = move_effect_journal_follow_piece_through_other_effects(nbply,
+                                                                                     id_influencer,
+                                                                                     sq_to);
+    piece_walk_type const walk_influencer = get_walk_of_piece_on_square(sq_arrival);
+    vec_index_type k;
+
+    for (k = vec_queen_start; k<=vec_queen_end; ++k)
+    {
+      square const sq_candidate = sq_arrival+vec[k];
+
+      if (TSTFLAG(being_solved.spec[sq_candidate],side_influenced)
+          && !TSTFLAG(being_solved.spec[sq_candidate],Royal))
+      {
+        boolean const prom_or_base = (TSTFLAG(sq_spec(sq_candidate),sq_prom)
+                                      || TSTFLAG(sq_spec(sq_candidate),sq_base));
+        piece_walk_type const walk_substitute = (prom_or_base
+                                                 ? walk_influencer
+                                                 : Pawn);
+
+        if (walk_substitute!=get_walk_of_piece_on_square(sq_candidate))
+          move_effect_journal_do_walk_change(move_effect_reason_influencer,
+                                             sq_candidate,
+                                             walk_substitute);
+      }
+    }
+  }
+
+  TraceFunctionExit(__func__);
+  TraceFunctionResultEnd();
+}
 
 /* Try to solve in solve_nr_remaining half-moves.
  * @param si slice index
@@ -30,46 +73,31 @@ void influencer_walk_changer_solve(slice_index si)
   move_effect_journal_index_type const movement = base+move_effect_journal_index_offset_movement;
   move_effect_journal_index_type curr;
 
-  Side const side_playing = trait[nbply];
-  Side const side_influenced = advers(side_playing);
-  SquareFlags const sq_prom = side_influenced==White ? WhPromSq : BlPromSq;
-  SquareFlags const sq_base = side_influenced==White ? WhBaseSq : BlBaseSq;
-
   TraceFunctionEntry(__func__);
   TraceFunctionParam("%u",si);
   TraceFunctionParamListEnd();
 
   for (curr = movement; curr!=top; ++curr)
-    if (move_effect_journal[curr].type==move_effect_piece_movement
-        && !TSTFLAG(move_effect_journal[curr].u.piece_movement.movingspec,Royal))
+    switch (move_effect_journal[curr].type)
     {
-      PieceIdType const moving_id = GetPieceId(move_effect_journal[curr].u.piece_movement.movingspec);
-      square const sq_to = move_effect_journal[curr].u.piece_movement.to;
-      square const sq_arrival = move_effect_journal_follow_piece_through_other_effects(nbply,
-                                                                                       moving_id,
-                                                                                       sq_to);
-      piece_walk_type const walk_moving = move_effect_journal[curr].u.piece_movement.moving;
-      vec_index_type k;
+      case move_effect_piece_movement:
+        if (!TSTFLAG(move_effect_journal[curr].u.piece_movement.movingspec,Royal))
+          apply_influence(GetPieceId(move_effect_journal[curr].u.piece_movement.movingspec),
+                          move_effect_journal[curr].u.piece_movement.to);
+        break;
 
-      for (k = vec_queen_start; k<=vec_queen_end; ++k)
-      {
-        square const sq_candidate = sq_arrival+vec[k];
+      case move_effect_piece_exchange:
+        if (!TSTFLAG(move_effect_journal[curr].u.piece_exchange.fromflags,Royal))
+          apply_influence(GetPieceId(move_effect_journal[curr].u.piece_exchange.fromflags),
+                          move_effect_journal[curr].u.piece_exchange.to);
 
-        if (TSTFLAG(being_solved.spec[sq_candidate],side_influenced)
-            && !TSTFLAG(being_solved.spec[sq_candidate],Royal))
-        {
-          boolean const prom_or_base = (TSTFLAG(sq_spec(sq_candidate),sq_prom)
-                                        || TSTFLAG(sq_spec(sq_candidate),sq_base));
-          piece_walk_type const walk_substitute = (prom_or_base
-                                                   ? walk_moving
-                                                   : Pawn);
+        if (!TSTFLAG(move_effect_journal[curr].u.piece_exchange.toflags,Royal))
+          apply_influence(GetPieceId(move_effect_journal[curr].u.piece_exchange.toflags),
+                          move_effect_journal[curr].u.piece_exchange.from);
+        break;
 
-          if (walk_substitute!=get_walk_of_piece_on_square(sq_candidate))
-            move_effect_journal_do_walk_change(move_effect_reason_influencer,
-                                               sq_candidate,
-                                               walk_substitute);
-        }
-      }
+      default:
+        break;
     }
 
   pipe_solve_delegate(si);
