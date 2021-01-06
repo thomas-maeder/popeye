a3880a639a21a3b5c93369e9958c79d2110fec78
Thomas Maeder <maeder+github@glue.ch>
2021-01-06 23:29:10 +0100

    improved consumption tracing

--- pieces/attributes/total_invisible/consumption.c
+++ pieces/attributes/total_invisible/consumption.c
@@ -9,16 +9,24 @@ dynamic_consumption_type current_consumption = { 0 };
 
 void TraceConsumption(void)
 {
-  TraceValue("%u",current_consumption.fleshed_out[White]);
-  TraceValue("%u",current_consumption.fleshed_out[Black]);
-  TraceValue("%u",current_consumption.placed[White]);
-  TraceValue("%u",current_consumption.placed[Black]);
-  TraceValue("%u",current_consumption.claimed[White]);
-  TraceValue("%u",current_consumption.claimed[Black]);
-  TraceValue("%u",static_consumption.pawn_victims[White]);
-  TraceValue("%u",static_consumption.pawn_victims[Black]);
-  TraceValue("%u",static_consumption.king[White]);
-  TraceValue("%u",static_consumption.king[Black]);
+  TraceValue("%u\n",current_consumption.fleshed_out[White]);
+  TraceValue("%u\n",current_consumption.fleshed_out[Black]);
+  TraceValue("%u\n",current_consumption.placed[White]);
+  TraceValue("%u\n",current_consumption.placed[Black]);
+  TraceValue("%u\n",current_consumption.claimed[White]);
+  TraceValue("%u\n",current_consumption.claimed[Black]);
+  TraceValue("%u\n",static_consumption.pawn_victims[White]);
+  TraceValue("%u\n",static_consumption.pawn_victims[Black]);
+  TraceValue("%u\n",static_consumption.king[White]);
+  TraceValue("%u\n",static_consumption.king[Black]);
+  TraceValue("%u\n",static_consumption.move_after_victim[White]);
+  TraceValue("%u\n",static_consumption.move_after_victim[Black]);
+  TraceSquare(being_solved.king_square[White]);TraceEOL();
+  TraceSquare(being_solved.king_square[Black]);TraceEOL();
+  TraceValue("%u\n",nr_total_invisbles_consumed_for_side(White));
+  TraceValue("%u\n",nr_total_invisbles_consumed_for_side(Black));
+  TraceValue("%u\n",total_invisible_number);
+  TraceEOL();
 }
 
 unsigned int nr_total_invisbles_consumed_for_side(Side side)
