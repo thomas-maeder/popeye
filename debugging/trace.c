ec24c45bf62aa98f1270cad836d0ddc9bc537ce1
Joshua Green <jvg1981@aim.com>
2021-03-28 21:55:41 -0400

    Additional sanity checking.

--- debugging/trace.c
+++ debugging/trace.c
@@ -133,11 +133,14 @@ void TraceFunctionParamListEnd(void)
 
 void TraceFunctionExit(char const *name)
 {
-  if (do_trace && (strcmp(name,entryNames[level])!=0))
-    fprintf(stderr,"Level:%lu Expected: %s. Got: %s\n",
-            level,entryNames[level],name);
+  if (level<max_nesting_level)
+  {
+    if (do_trace && (strcmp(name,entryNames[level])!=0))
+      fprintf(stderr,"Level:%lu Expected: %s. Got: %s\n",
+              level,entryNames[level],name);
 
-  assert(strcmp(name,entryNames[level])==0);
+    assert(strcmp(name,entryNames[level])==0);
+  }
 
   if (do_trace && (level<=max_level))
   {
