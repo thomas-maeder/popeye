97e7ca75fbf96dea7702d56024e853112676016a
Joshua Green <jvg1981@aim.com>
2021-04-25 18:14:45 -0400

    Restoring the use of the special _WIN32/__CYGWIN__ code when needed.

--- utilities/boolean.h
+++ utilities/boolean.h
@@ -1,16 +1,6 @@
 #if !defined(UTILITIES_BOOLEAN_H)
 #define UTILITIES_BOOLEAN_H
 
-#if defined(__cplusplus) || (defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L))
-
-#if !defined(__cplusplus)
-#include <stdbool.h>
-#endif
-
-typedef bool boolean;
-
-#else
-
 #if defined(_WIN32) || defined(__CYGWIN__)
 
 /* avoid clash with type boolean defined in: */
@@ -22,6 +12,14 @@ enum
   true
 };
 
+#elif defined(__cplusplus) || (defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L))
+
+#if !defined(__cplusplus)
+#include <stdbool.h>
+#endif
+
+typedef bool boolean;
+
 #else
 
 typedef enum
@@ -29,8 +27,6 @@ typedef enum
   false,
   true
 } boolean;
-
-#endif
 
 #endif
 
