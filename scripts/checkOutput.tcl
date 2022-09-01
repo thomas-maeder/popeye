3966c7b3a114da238e22757a966d2661dd996cd1
Thomas Maeder <thomas-maeder@users.noreply.github.com>
2022-09-01 07:09:31 +0200

    restrict namespace german to German elements

--- scripts/checkOutput.tcl
+++ scripts/checkOutput.tcl
@@ -20,70 +20,70 @@ switch -re $inputfile {
 }
 
 namespace eval german {
-    namespace eval intro {
-	set emptyLine {\n}
-	set textLine { +[^\n]+\n}
-	set combined "$emptyLine+(?:$textLine)*"
-    }
-
-    namespace eval board {
-        set nrRows 8
-        set nrColumns 8
-    
-	set emptyLine {\n}
-
-	set cornerSign {[+]}
-	set verticalBorderSign {[|]}
-	set horizontalBorderSign "-"
-	
-	set columnName {[a-h]}
-	set columnSpec "$horizontalBorderSign$horizontalBorderSign$columnName$horizontalBorderSign"
-	set columns "$cornerSign${horizontalBorderSign}(?:$columnSpec){$nrColumns}$horizontalBorderSign$horizontalBorderSign$cornerSign\n"
-
-	set spaceLine "$verticalBorderSign (?:    ){$nrColumns}  $verticalBorderSign\n"
-
-	set rowNo {[1-8]}
-	set squareEmpty {  [.]}
-	set color {[ =-]}
-	set pieceLetter {[[:upper:]]}
-	set piece1Letter " $color$pieceLetter"
-	set piece2Letters "$color$pieceLetter{2}"
-	set pieceSpec "(?:$squareEmpty|$piece1Letter|$piece2Letters)"
-	set piecesLine "$rowNo (?:$pieceSpec ){$nrColumns}  $rowNo\n"
-
-	set stipulation {\#[[:digit:]]+}
-	set piecesOfColor {[[:digit:]]+}
-	set plus {[+]}
-	set pieceControl "$piecesOfColor $plus $piecesOfColor"
-	set captionLine "  $stipulation +$pieceControl\n"
-	
-	set combined "$emptyLine${columns}(?:$spaceLine$piecesLine){$nrRows}$spaceLine$columns$captionLine"
-    }
-
-    namespace eval conditions {
-	set line { +[^\n]+\n}
-	set combined "(?:$line)*"
-    }
-
-    namespace eval solution {
-	set emptyLine {\n}
-	set combined "$emptyLine"
-    }
-
-    set bodyRest {.+?}
-    
     set endlines {Loesung beendet[.]\n+?}
-    
-    set problem "($intro::combined$board::combined$conditions::combined$solution::combined)${bodyRest}($endlines)"
-
-    set problems "(?:$problem)+?"
 }
+
+namespace eval intro {
+    set emptyLine {\n}
+    set textLine { +[^\n]+\n}
+    set combined "$emptyLine+(?:$textLine)*"
+}
+
+namespace eval board {
+    set nrRows 8
+    set nrColumns 8
+    
+    set emptyLine {\n}
+
+    set cornerSign {[+]}
+    set verticalBorderSign {[|]}
+    set horizontalBorderSign "-"
+    
+    set columnName {[a-h]}
+    set columnSpec "$horizontalBorderSign$horizontalBorderSign$columnName$horizontalBorderSign"
+    set columns "$cornerSign${horizontalBorderSign}(?:$columnSpec){$nrColumns}$horizontalBorderSign$horizontalBorderSign$cornerSign\n"
+
+    set spaceLine "$verticalBorderSign (?:    ){$nrColumns}  $verticalBorderSign\n"
+
+    set rowNo {[1-8]}
+    set squareEmpty {  [.]}
+    set color {[ =-]}
+    set pieceLetter {[[:upper:]]}
+    set piece1Letter " $color$pieceLetter"
+    set piece2Letters "$color$pieceLetter{2}"
+    set pieceSpec "(?:$squareEmpty|$piece1Letter|$piece2Letters)"
+    set piecesLine "$rowNo (?:$pieceSpec ){$nrColumns}  $rowNo\n"
+
+    set stipulation {\#[[:digit:]]+}
+    set piecesOfColor {[[:digit:]]+}
+    set plus {[+]}
+    set pieceControl "$piecesOfColor $plus $piecesOfColor"
+    set captionLine "  $stipulation +$pieceControl\n"
+    
+    set combined "$emptyLine${columns}(?:$spaceLine$piecesLine){$nrRows}$spaceLine$columns$captionLine"
+}
+
+namespace eval conditions {
+    set line { +[^\n]+\n}
+    set combined "(?:$line)*"
+}
+
+namespace eval solution {
+    set emptyLine {\n}
+    set combined "$emptyLine"
+}
+
+set bodyRest {.+?}
+
+set problem "($intro::combined$board::combined$conditions::combined$solution::combined)${bodyRest}([set ${language}::endlines])"
+
+set problems "(?:$problem)+?"
 
 set f [open $inputfile "r"]
 set input [read $f]
 close $f
 
-set matches [regexp -all -inline [set ${language}::problems] $input]
+set matches [regexp -all -inline $problems $input]
 
 foreach { whole top bottom } $matches {
     puts -nonewline $top
