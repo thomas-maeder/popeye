23bb8e0cf75eaf9921ad72e5b9c47f999954b9ba
Thomas Maeder <maeder@glue.ch>
2023-12-12 19:51:26 +0100

    separate module for the grammar parser

--- scripts/checkOutput.tcl
+++ scripts/checkOutput.tcl
@@ -3,6 +3,7 @@
 source output/plaintext/documentation/german
 source output/plaintext/documentation/english
 source output/plaintext/documentation/cmdline.tcl
+source output/plaintext/documentation/grammarParser.tcl
 
 set languagesRE [join [regsub -all ::language:: [namespace children ::language] ""] "|"]
 
@@ -33,64 +34,7 @@ try {
     exit 1
 }
 
-namespace eval ::grammarParser {
-    namespace export literal terminal nonterminal lookupName scope
-}
-
-proc ::grammarParser::scope {name code} {
-    uplevel namespace eval $name "namespace import ::grammarParser::*"
-    uplevel namespace eval $name [list $code]
-}
-
-proc ::grammarParser::literal {name} {
-    return [set ::language::${::params(language)}::$name]
-}
-
-proc ::grammarParser::terminal {name expression} {
-    upvar $name result
-
-    set result [list "terminal" $expression]
-}
-
-proc ::grammarParser::lookupName {name} {
-    set scope [uplevel namespace current]
-    set initialscope $scope
-    while {![info exists ${scope}::$name]} {
-        if {$scope=="::grammar"} {
-            error "can't find variable $name from scope $initialscope"
-        } else {
-            set scope [namespace parent $scope]
-        }
-    }
-    return ${scope}::$name
-}
-
-proc ::grammarParser::nonterminal {name production} {
-    upvar $name result
-
-    regsub -all {[[:space:]]+} [string trim $production] " " production
-
-    set lookedup ""
-    foreach token [split $production " "] {
-	if {[regexp -- {^([[:alnum:]_:]+)([?*+]|{.*})?$} $token - name quantifier]} {
-	    set name [uplevel lookupName $name]
-	    lappend lookedup [list "name" $name $quantifier]
-	} else {
-	    lappend lookedup [list "operator" $token ""]
-	}
-    }
-
-    set result [list "nonterminal" $lookedup]
-}
-
-if {[catch {
-    ::grammarParser::scope grammar {
-	source output/plaintext/documentation/grammar
-    }
-} error]} {
-    puts $errorInfo
-    exit 1
-}
+::grammarParser::parse output/plaintext/documentation/grammar ::grammar
 
 proc resolve {name} {
     global resolved
