1a21b674183b0a485ff6ea026532b7e086e93626
Thomas Maeder <maeder@glue.ch>
2024-03-01 22:07:09 +0100

    #382 a Bul hurdle move is illegal if there is no Bul movement for the hurdle

--- pieces/attributes/bul.c
+++ pieces/attributes/bul.c
@@ -4,11 +4,17 @@
 #include "solving/pipe.h"
 #include "solving/move_generator.h"
 #include "solving/move_effect_journal.h"
+#include "solving/has_solution_type.h"
 #include "stipulation/move.h"
 #include "debugging/trace.h"
 #include "pieces/pieces.h"
 
 #include "debugging/assert.h"
+
+static boolean is_false(numecoup n)
+{
+  return false;
+}
 
 /* Try to solve in solve_nr_remaining half-moves.
  * @param si slice index
@@ -39,38 +45,49 @@ void bul_solve(slice_index si)
     square const sq_hurdle = hoppper_moves_auxiliary[move_generation_stack[CURRMOVE_OF_PLY(nbply)].id].sq_hurdle;
     piece_walk_type const pi_hurdle = get_walk_of_piece_on_square(sq_hurdle);
     ply const save_nbply = nbply;
+    ply const bul_ply = nbply+1; /* this assumption is not always correct */
 
     TraceSquare(sq_hurdle);
     TraceWalk(pi_hurdle);
     TraceEOL();
 
     siblingply(advers(SLICE_STARTER(si)));
-    assert(nbply==save_nbply+1);
+    numecoup const start = CURRMOVE_OF_PLY(nbply);
+    assert(nbply==bul_ply);
     curr_generation->departure = sq_hurdle;
     move_generation_current_walk = walk_moving;
     generate_moves_delegate(SLICE_NEXT2(temporary_hack_move_generator[trait[nbply]]));
+    move_generator_filter_captures(start,&is_false); /* is this correct??? */
 
-    nbply = save_nbply;
-
+    if (start==CURRMOVE_OF_PLY(nbply))
     {
-      numecoup const curr = CURRMOVE_OF_PLY(nbply+1);
-      move_generation_elmt const * const move_gen_top = move_generation_stack+curr;
-      square const sq_capture = move_gen_top->capture;
-      square const sq_departure = move_gen_top->departure;
-      square const sq_arrival = move_gen_top->arrival;
-
-      TraceSquare(sq_departure);
-      TraceSquare(sq_arrival);
-      TraceSquare(sq_capture);
-      TraceEOL();
-
-      move_effect_journal_do_piece_movement(move_effect_reason_bul,sq_departure,sq_arrival);
+      solve_result = this_move_is_illegal;
+      finply();
     }
+    else
+    {
+      nbply = save_nbply;
 
-    pipe_solve_delegate(si);
-    ++nbply;
+      {
+        numecoup const curr = CURRMOVE_OF_PLY(bul_ply);
+        move_generation_elmt const * const move_gen_top = move_generation_stack+curr;
+        square const sq_capture = move_gen_top->capture;
+        square const sq_departure = move_gen_top->departure;
+        square const sq_arrival = move_gen_top->arrival;
 
-    finply();
+        TraceSquare(sq_departure);
+        TraceSquare(sq_arrival);
+        TraceSquare(sq_capture);
+        TraceEOL();
+
+        move_effect_journal_do_piece_movement(move_effect_reason_bul,sq_departure,sq_arrival);
+      }
+
+      pipe_solve_delegate(si);
+      nbply = bul_ply;
+
+      finply();
+    }
   }
   else
     pipe_solve_delegate(si);
