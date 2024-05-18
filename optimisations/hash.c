bcbbe7381c27742ddbe3cece726b6e2d59be52bb
Joshua Green <jvg1981@aim.com>
2024-05-18 08:55:25 -0400

    Switching to the idiomatic expression.

--- optimisations/hash.c
+++ optimisations/hash.c
@@ -2485,7 +2485,7 @@ static dhtElement *find_or_add_help_elmt(HashBuffer const *hb)
     dhtElement *result;
     k.value.object_pointer = &hb->cmv;
     result = dhtLookupElement(pyhash,k);
-    if (!result)
+    if (result==dhtNilElement)
       result = allocDHTelement(k);
     TraceFunctionExit(__func__);
     TraceFunctionResult("%p",(void const *)result);
